491786b5e4349139869529cc7735eb9346d4957a
ridhoi muhamad <ridho007@outlook.co.id>
2017-03-01 09:07:02 +0700

    phase0-week2

---
+++ assignment.js
@@ -0,0 +1,19 @@
+/////////////////////////////
+// Phase 0 Week 2
+///////////////////// javascript
+
+var 
+    number1=33;
+    number2=6;
+    string1='foo';
+    string2='bar';
+    boolean1=true;
+    boolean2=false;
+{
+	console.log("angka 1 adalah =" +number1)
+	console.log("angka 2 adalah =" +number2)
+	console.log("string 1 adalah =" +string1)
+	console.log("string 2 adalah = "+string2)
+	console.log("boolean 1 adalah = "+boolean1)
+	console.log("boolean 2 adalah = "+boolean2)
+}
