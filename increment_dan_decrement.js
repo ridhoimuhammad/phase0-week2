7717e42692e623b61c483e40f09b37a8fc7847ab
ridhoi muhamad <ridho007@outlook.co.id>
2017-03-01 21:59:48 +0700

    javascript

---
+++ increment_dan_decrement.js
@@ -0,0 +1,10 @@
+////////////////////////////
+// Phase 0 week 2
+// Looping Menggunakan For
+//////////////////////// increment_dan_decrement.js
+for (var angka = 1; angka <= 20; angka++) {
+  console.log(angka +' -I love coding');
+}
+for (var angka = 20; angka >= 1; angka--) {
+  console.log(angka +' -I will become fullstack developer');
+}
