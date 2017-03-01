cf88bf88cc542b1f98ec2e228f12b7219f23255a
ridhoi muhamad <ridho007@outlook.co.id>
2017-03-01 22:59:21 +0700

    javascript

---
+++ pseudocode.js
@@ -0,0 +1,26 @@
+///////////////////
+/// pseudocode
+/////////////////////// pseudocode.js
+
+//TUGAS 1
+
+//memperkenalan angka pada mesin
+angka1,angka2,angka3
+
+angka1 SIMPAN
+angka2 SIMPAN
+angka3 = HITUNG[angka1 dikali angka2]
+SIMPAN ke angka3
+TULISKAN (angka3)
+
+//TUGAS 2
+input KATA lalu SIMPAN
+	IF KATA ===  "Saya Belajar JavaScript!"
+		TULISKAN "Pasti Bisa!"
+	ELSE 
+		TULISKAN "Semangat!"
+
+//TUGAS 3
+ iput = ... SIMPAN
+(i=ambil_input,i habis_dibagi 3) ULANGI
+TAMPILKAN
