70260654731faca35caa1f68507ebfbad02388f8
ridhoi muhamad <ridho007@outlook.co.id>
2017-03-02 00:22:02 +0700

    javascript

---
+++ function.js
@@ -0,0 +1,27 @@
+//tugas1 1
+
+function shoutOut(){
+	console.log("halo!");
+}
+console.log(shoutOut())
+
+//tugas 2
+function calculateMultiply(){
+return num1*num2;
+}
+var num1 = 5;
+var num2 = 6;
+var hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian);
+
+ //tugas 3
+function processSentence(){
+return 'Nama Saya ' +name+',umur saya '+age+'tahun, alamat saya di'+address +',dan saya punya hobby yaitu '+hobby;
+}
+var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogjakarta";
+var hobby = "gaming";
+
+var fullSentence = processSentence(name,age,address,hobby);
+console.log(fullSentence);
