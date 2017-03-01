3d9786677cf20095efb8d0d4da96722f2ac0a5a9
ridhoi muhamad <ridho007@outlook.co.id>
2017-03-01 13:42:39 +0700

    phase0-week2

---
+++ Proxytia_Game.js
@@ -0,0 +1,40 @@
+////////////////////////////
+// Phase 0 week 2
+// logika if-else
+//////////////////////// proxytia_games.js
+
+console.log("+-------------------------------+")
+console.log("+W E L C O M E  to Proxytia game+")
+console.log("+--------- P E R A N -----------+")
+console.log("|1. Ksatria, 2.Tabib, 3.Penyihir|")
+console.log("|******* Case-sensitive ********|")
+console.log("+-------------------------------+")
+//Penulisan peran case-sensitive
+
+var nama,peran;
+nama='i';
+peran='Tabib';
+if(nama==='',peran===''){
+  console.log("Nama tidak boleh kosong")
+  console.log("Pilih peranmu untuk memulai game")
+}
+else if(nama===+nama,peran==='Ksatria')
+{  
+console.log("Selamat datang di Dunia Proxytia, " +nama)
+console.log("Halo Ksatria  "+nama, " kamu dapat menyerang dengan senjatamu!")
+}
+else if(nama===+nama,peran==='Tabib')
+{  
+console.log("Selamat datang di Dunia Proxytia, " +nama)
+console.log("Halo Tabib "+nama," kamu akan membantu temanmu yang terluka.")
+}
+else if(nama===+nama,peran==='Penyihir')
+{  
+console.log("Selamat datang di Dunia Proxytia, " +nama)
+console.log("Halo Penyihir "+nama," ciptakan keajaiban yang membantu kemenanganmu!")
+}
+else
+{  
+console.log("Selamat datang di Dunia Proxytia, " +nama)
+console.log("Halo..."+nama,"typing berpengaruh, Program ini Case-sensitive :D :D ")
+}
