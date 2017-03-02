aeb27ef3105db216d735633ccc767583990ea0a0
ridhoi muhamad <ridho007@outlook.co.id>
2017-03-02 09:29:15 +0700

    pseudocode_javascript

---
+++ tugas1.js
@@ -0,0 +1,10 @@
+////////////////
+/// Psuedocode 
+///////////////////
+// T U G A S - 1
+/////////////////////// tugas.js
+var 
+nama='eky';
+angkapembilang=20;
+angkapenyebut=10;
+console.log('halo. . .'+nama +'  angka pembilang dibagi angka penyebut adalah = ' +angkapembilang / +angkapenyebut)
