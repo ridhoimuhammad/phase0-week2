f6049239557a8e18d3d3b522d309aea2c3558a41
ridhoi muhamad <ridho007@outlook.co.id>
2017-03-02 14:08:27 +0700

    javascript

--- tugas3.js
+++ tugas3.js
@@ -3,15 +3,16 @@
 ///////////////////
 // T U G A S - 2
 /////////////////////// tugas3.js
-var tahun=7;
-if(tahun % 2 === 0){
+var tahun=2017;
+if(tahun %4 ===0 )
+else if(tahun % 100 === 0){
 	console.log('tahun kabisat')
 }
 else 
 {
 	console.log('bukan tahun kabisat')
 }
-else if(tahun % 4 === 0){
+else if(tahun % 400 === 0){
 	console.log('tahun kabisat')
 }
 else{
