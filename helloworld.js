1888e8d47e642a6729e64aad26c62096da68e0a3
ridhoi muhamad <ridho007@outlook.co.id>
2017-03-02 16:42:15 +0700

    javascript-array

---
+++ helloworld.js
@@ -0,0 +1,4 @@
+var input ='hello word'
+console.log(input.charAt(10)+input.charAt(9)+input.charAt(8)+input.charAt(7)+input.charAt(6)
+	+input.charAt(5)+input.charAt(4)+input.charAt(3)+input.charAt(2)+input.charAt(1)
+	+input.charAt(0));
