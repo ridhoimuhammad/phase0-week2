9d4535f3badc364711801978fa4ce203f9928217
ridhoi muhamad <ridho007@outlook.co.id>
2017-03-03 08:02:19 +0700

    looping Array

---
+++ loopArray.js
@@ -0,0 +1,10 @@
+var input = [
+                ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+                ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+                ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+                ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+            ]
+console.log('Nomor ID: ')
+console.log('Nama Lengkap: ')
+console.log('TTL: ')
+console.log('Hobi: ')
