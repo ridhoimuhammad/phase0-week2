c26f45e7ab13779bffec6f90108aea8e56a82af5
ridhoi muhamad <ridho007@outlook.co.id>
2017-03-03 08:23:06 +0700

    looping Array

--- loopArray.js
+++ loopArray.js
@@ -1,10 +1,18 @@
-var input = [
-                ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
-                ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
-                ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
-                ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
-            ]
-console.log('Nomor ID: ')
+////////////////////////////
+// Phase 0 week 2
+// Looping Array
+//////////////////////// loopingArray.js
+
+var input = 
+[["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+ ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+ ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+ ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]]
+
+for (var angka = 1; angka <= 4; angka++) {
+console.log('Nomor ID    :' +input[0][0]);
 console.log('Nama Lengkap: ')
-console.log('TTL: ')
-console.log('Hobi: ')
+console.log('TTL         : ')
+console.log('Hobi        : ')
+console.log('----------------------------------------------')
+}
