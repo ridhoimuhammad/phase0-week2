fa7e219e798fabc4b186d382965b49f4cafdab0f
ridhoi muhamad <ridho007@outlook.co.id>
2017-03-04 12:26:46 +0700

    json

---
+++ hewan.js
@@ -0,0 +1,13 @@
+var hewanHidup = {
+	id: "amonia007",
+	name: "Macan",
+	age: 12,
+	eat_favorites: [
+		"kelinci",
+		"kadal",
+		{
+			keistimewaan: ["lari cepat","cakar tajam"]
+		}
+	],
+	alamat: {}
+};
