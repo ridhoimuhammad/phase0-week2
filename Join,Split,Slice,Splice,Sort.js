e5db911f0e384cf59991354912435152d982b7d7
ridhoi muhamad <ridho007@outlook.co.id>
2017-03-08 16:44:37 +0700

    belajar js sama parel

--- Join,Split,Slice,Splice,Sort.js
+++ Join,Split,Slice,Splice,Sort.js
@@ -1,25 +1,31 @@
 function dataHandling2(){
   var output = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"];
-  output.splice(4,5, "Pria","SMA Internasional Metro");
+  output.splice(4,1, "Pria","SMA Internasional Metro");
   console.log(output);
-  var baru = (output[3]);
-  var baru1 = (output[3]);
-  if(baru || '21/05/1989'){
-    console.log('Mei');
+  var tanggal= output[3];
+  //console.log(tanggal);
+  var tanggal2= tanggal.split('/');
+  //console.log(tanggal2);
+  var bulan = tanggal2[1];
+  //console.log(bulan);
+  var bulan2 = parseInt(bulan);
+  //console.log(bulan2);
+  if(bulan2 == 1){
+    console.log("January")
+  }else if(bulan2 == 2){
+    console.log("February")
+  }else if(bulan2 == 3){
+    console.log("Maret")
+  }else if(bulan2 == 4){
+    console.log("April")
+  }else if(bulan2 == 5){
+    console.log("Mei")
   }
-  else{
-    console.log("tidak ada");
-  }
-  var baru3=baru1.split("");
-  baru3.sort(function(value1, value2) { return value1 < value2 });
-  console.log(baru3);
-  var baru4=(output[3]);
-  //console.log(baru4);
-  var baru5=baru4.split('');
-  var baru6=baru5.join(" ");
-  //var baru7=baru6.splice(4,10,"-","-");
-  console.log(baru6);
-  var baru7=output.slice(1,2);
-  console.log(baru7);
+  var s=tanggal2.sort(function(value1, value2) { return value1 < value2 });
+  console.log(s);
+  var a = s.join("-");
+  console.log(a);
+  //console.log([1,11,2,21,3].sort());
+  console.log(output[1]);
 }
 dataHandling2();
