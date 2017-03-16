dc858da74b097f94cbab3391efdd0aa28e5d342b
ridhoi muhamad <ridho007@outlook.co.id>
2017-03-16 12:17:19 +0700

    belajar

--- json.js
+++ json.js
@@ -13,21 +13,3 @@ var Object1 = {
     }
   ],
 };
-console.log(Object1.hari[0]+' '+Object1.kehadiran[0]);
-console.log(Object1.hari[1]+' '+Object1.kehadiran[1]);
-console.log(Object1.hari[2]+' '+Object1.kehadiran[2]+' alasan '+Object1.kehadiran[3].alasan);
-console.log('***********************************')
-var kehadiran =[["Senin","Masuk"," - "],["Rabu","Masuk"," - "],["Jumat","Absen","Dinas Luar"]]
-for (var absensi = 0; absensi < 3; kehadiran,absensi++){
-  console.log('Hari      : ' +kehadiran[absensi][0])
-  console.log('Kehadiran : ' +kehadiran[absensi][1])
-  console.log('Alasan    : ' +kehadiran[absensi][2])
-  console.log('--------------------------------')
-}
-var masuk = 2,absen = 1,jumlah = absensi;
-console.log("Total Hari kerja minggu ini :" +jumlah +" hari")
-//if(kehadiran){
-//console.log(kehadiran[absensi][0])
-//}
-console.log("Total Masuk : " +masuk +" hari")
-console.log("Total Tidak Masuk : " +absen +" hari")
---
+++ roket.js
@@ -0,0 +1,31 @@
+// phase 0 week 2
+// ganjil genap
+// --------R O K E T-----
+for(var i = 1; i<=100; i++){
+if(i%2===0){
+  console.log(i+"angka genap")
+}else{
+  console.log(i+"angka ganjil")
+}
+}
+for(var i = 1; i<=100;i=i+2){
+  if(i%3===0){
+    console.log(i+' kelipatan '+i);
+}else{
+  console.log(i+" ");
+}
+}
+for(var i = 1; i<=100;i=i+5){
+  if(i=i+5){
+    console.log(i+' kelipatan '+i);
+}else if (i!==i+5){
+  console.log(i+" ");
+}
+}
+for(var i = 1; i<=100;i=i+9){
+  if(i=i+9){
+    console.log(i+' kelipatan '+i);
+}else if (i!==i+9){
+  console.log(i+" ");
+}
+}
