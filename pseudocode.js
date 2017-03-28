e85b0fa3afb804313b67550bc01418992e427de1
ridhoi muhamad <ridho007@outlook.co.id>
2017-03-28 18:19:35 +0700

    7

--- pseudocode.js
+++ pseudocode.js
@@ -7,20 +7,25 @@
 //memperkenalan angka pada mesin
 angka1,angka2,angka3
 
-angka1 SIMPAN
-angka2 SIMPAN
-angka3 = HITUNG[angka1 dikali angka2]
-SIMPAN ke angka3
+BACA angka SIMPAN pada angka1
+BACA angka SIMPAN pada angka2
+HITUNG[angka1 dikali angka2]SIMPAN ke angka3
+
 TULISKAN (angka3)
 
 //TUGAS 2
 input KATA lalu SIMPAN
 	IF KATA ===  "Saya Belajar JavaScript!"
 		TULISKAN "Pasti Bisa!"
-	ELSE 
+	ELSE
 		TULISKAN "Semangat!"
 
 //TUGAS 3
- iput = ... SIMPAN
-(i=ambil_input,i habis_dibagi 3) ULANGI
-TAMPILKAN
+ iput = angka SIMPAN
+ var input = 10;
+ for(var i=1;i<input;i++){
+   if(i%3===0){
+  TAMPILKAN(i);
+		 ULANGI
+   }
+ }
