49805d0b7ff5c0d19c4b16c919423af7403b565c
ridhoi muhamad <ridho007@outlook.co.id>
2017-03-28 22:32:36 +0700

    7

--- loopArray.js
+++ loopArray.js
@@ -3,17 +3,28 @@
 // Looping Array
 //////////////////////// loopingArray.js
 
-var input = 
+////////////////////////////
+// Phase 0 week 2
+// Looping Array
+//////////////////////// loopingArray.js
+
+////////////////////////////
+// Phase 0 week 2
+// Looping Array
+//////////////////////// loopingArray.js
+function dataHandling(){
+var input =
 [["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
  ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
  ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
  ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]]
 
 for (var angka = 0; angka < 4; input,angka++){
-console.log('Nomor ID    :' +input[angka][0])
-console.log('Nama Lengkap: ' +input[angka][1])
-console.log('TTL         : ' +input[angka][2])
-console.log('Hobi        : ' +input[angka][3])
+console.log('Nomor ID     :' +input[angka][0])
+console.log('Nama Lengkap : ' +input[angka][1])
+console.log('TTL          : ' +input[angka].slice(2,4))
+console.log('Hobi         : ' +input[angka][4])
 console.log('----------------------------------------------')
 }
-
+}
+dataHandling();
